a82bfbfed3637c2977bec8bf5182d01bd67b162e
Brenek Harrison <brenekharrison@gmail.com>
2022-04-27 16:53:55 -0600

    Write out new main() approach

--- update-package-via-manifest/src/index.ts
+++ update-package-via-manifest/src/index.ts
@@ -1,14 +1,21 @@
 import * as core from "@actions/core"
 import * as github from "@actions/github"
-import { promise as glob } from "glob-promise"
-import * as fs from "fs"
 import * as path from "path"
-import { execSync } from "child_process"
-import axios from "axios"
+import * as fs from "fs"
+import { promise as glob } from "glob-promise"
 
-try {
-	const context = github.context
+interface IManifest {
+	name: string,
+	testCmd: string | null,
+	include: string[],
+	automaticUpdates: {
+		type: string,
+		repo: string,
+		appID: string,
+	}
+}
 
+async function main() {
 	// Identify all packages in the pkgs directory
 	const manifests = await glob("pkgs/**/.aurmanifest.json")
 
@@ -19,111 +26,34 @@ try {
 
 		const manifest = JSON.parse(fs.readFileSync(manifestPath).toString()) as IManifest
 
+		// TODO: Identify which updateProvider to use
+
 		const pkgbuildVersion: string | undefined = getVersionFromPKGBUILD(manifestPath.replace("/.aurmanifest.json", ""))
 		if (pkgbuildVersion === undefined) {
 			core.warning(`Unable to get package version from PKGBUILD (${manifest.name})`)
 			continue
 		}
 
-		let latestVersion = ""
-		switch (manifest.automaticUpdates.type) {
-			case "github":
-				const ghVersion = await getLatestVersionFromGithub(manifest.automaticUpdates.repo)
-				if (ghVersion === undefined) {
-					core.warning(`Failed to get latest version from GitHub (${manifestPath}).`)
-					continue
-				}
-				latestVersion = ghVersion.replace(/^v/m, "") // Remove leading v idiom
-				break
-			case "equinox":
-				const eqVer = await getLatestVersionFromEquinox(manifest.automaticUpdates.appID)
-				if (eqVer === undefined) {
-					core.warning(`Failed to get latest version from Equinox (${manifestPath}).`)
-					continue
-				}
-				latestVersion = eqVer
-				break
-			case undefined: // Skip if the automaticUpdates field is empty
-				continue
-			default:
-				core.warning(`Unknown automaticUpdates type '${manifest.automaticUpdates.type}' in ${manifestPath}`)
-				continue
-		}
+		// TODO: Ask updateProvider what the latest version is
 
-		core.info(`PKGBUILD: '${pkgbuildVersion}' Latest: '${latestVersion}'`)
-		if (pkgbuildVersion === latestVersion) {
-			continue
-		}
+		// TODO: Check if PKGBUILD and latest versions are the same
 
-		// Change permissions so that everything "should be" writable and so git won't complain
-		// about an unsafe directory
-		execSync("sudo chown -R builder:builder $(pwd)", { stdio: 'inherit' })
+		// TODO: Check if a PR has already been opened
 
-		core.info(execSync("git remote -v").toString())
+		// TODO: Ask updateProvider for the update data to act upon
 
-		// Check current branches for latestVersion
-		if (hasVersionAlreadyBeenPushed(manifest.name, latestVersion)) {
-			continue
-		}
+		// TODO: Update PKGBUILD with new version
 
-		// Create new branch
-		const branchName = `bot/${manifest.name}/${latestVersion}`
-		execSync(`git checkout -b ${branchName}`, { stdio: 'inherit' })
+		// TODO: Update PKGBUILD source arrays with sources (if provided by updateProvider)
 
-		let pkgbuildContents: string = fs.readFileSync(pkgbuildPath).toString()
+		// TODO: Write PKGBUILD changes to disk
 
-		// Update PKGBUILD with new version (pkgver to latestVersion and pkgrel to 1)
-		pkgbuildContents = pkgbuildContents.replace(/^pkgver=.*/m, `pkgver=${latestVersion}`)
-		pkgbuildContents = pkgbuildContents.replace(/^pkgrel=.*/m, "pkgrel=1")
+		// TODO: Run updpkgsums if requested by updateProvider
 
-		fs.writeFileSync(pkgbuildPath, pkgbuildContents)
+		// TODO: Commit and push changes
 
-		// Only update the checksums in PKGBUILD when the manifest type supports it.
-		//
-		// For example, GitHub urls will update cleanly with the package version, but
-		// equinox.io uses random strings in their urls that have to be updated manually.
-		// To prevent errors from updpkgsums, we skip the checksum update and rely on
-		// maintainers to do it properly.
-		if (["github"].indexOf(manifest.automaticUpdates.type) !== -1) {
-			// Update checksums in PKGBUILD
-			execSync(`updpkgsums`, {
-				stdio: "inherit",
-				cwd: manifestPath.replace("/.aurmanifest.json", ""),
-			})
-		}
-
-		// git add and commit updated PKGBUILD
-		execSync(`git add ${pkgbuildPath}`, { stdio: 'inherit' })
-		execSync(`git commit -m "Update ${manifest.name} to ${latestVersion}"`, {
-			stdio: "inherit",
-			env: {
-				GIT_AUTHOR_NAME: "github-actions[bot]",
-				GIT_AUTHOR_EMAIL: "41898282+github-actions[bot]@users.noreply.github.com",
-				GIT_COMMITTER_NAME: "github-actions[bot]",
-				GIT_COMMITTER_EMAIL: "41898282+github-actions[bot]@users.noreply.github.com",
-				EMAIL: "41898282+github-actions[bot]@users.noreply.github.com",
-			}
-		})
-
-		// Push changes to GitHub
-		execSync(`git push origin ${branchName}`, { stdio: 'inherit' })
-
-		// Create a pull request with the changes
-		const octokit = github.getOctokit(core.getInput("github-token"))
-		octokit.rest.pulls.create({
-			...context.repo,
-			head: branchName,
-			base: "master",
-			maintainer_can_modify: true,
-			title: `Update ${manifest.name} to ${latestVersion}`,
-		})
-
-		// Switch back to master branch
-		execSync(`git checkout master`, { stdio: 'inherit' })
+		// TODO: Open PR with any custom text from updateProvider
 	}
-
-} catch (error: any) {
-	core.setFailed(error)
 }
 
 function getVersionFromPKGBUILD(dir: string): string | undefined {
@@ -137,76 +67,8 @@ function getVersionFromPKGBUILD(dir: string): string | undefined {
 	return matches[1]
 }
 
-async function getLatestVersionFromGithub(repo: string): Promise<string | undefined> {
-	const split = repo.split("/")
-	const owner = split[0]
-	const repoName = split[1]
-
-	if (owner === undefined || repoName === undefined) {
-		return undefined
-	}
-
-	const octokit = github.getOctokit(core.getInput("github-token"))
-
-	let resp;
-	try {
-		resp = await octokit.rest.repos.getLatestRelease({
-			owner: owner,
-			repo: repoName,
-		})
-	} catch (e: any) {
-		core.warning(e)
-		return undefined
-	}
-
-	if (resp.status !== 200) {
-		return undefined
-	}
-
-	return resp.data.tag_name
-}
-
-async function getLatestVersionFromEquinox(appID: string): Promise<string | undefined> {
-	// Send request to equinox.io
-	const result = await axios.post("https://update.equinox.io/check", {
-		"app_id": appID,
-		"channel": "stable",
-		"current_sha256": "",
-		"current_version": "0.0.0",
-		"goarm": "",
-		"os": "linux",
-		"target_version": "",
-		"arch": "amd64"
-	})
-
-	if (result.status !== 200) {
-		core.warning(`Received non-200 status code from update.equinox.io. Status: ${result.statusText}(${result.status}). Data: ${result.data}`)
-		return undefined
-	}
-
-	return result.data.release.version
-}
-
-interface IManifest {
-	name: string,
-	testCmd: string | null,
-	include: string[],
-	automaticUpdates: {
-		type: string,
-		repo: string,
-		appID: string,
-	}
-}
-
-function hasVersionAlreadyBeenPushed(packageName: string, version: string): boolean {
-	const remoteBranches: string = execSync("git ls-remote --heads origin").toString()
-
-	for (const branch of remoteBranches.split("\n")) {
-		if (branch.indexOf(`${packageName}/${version}`) === -1) {
-			continue
-		}
-		return true
-	}
-
-	return false
+try {
+	await main()
+} catch (error: any) {
+	core.setFailed(error)
 }
