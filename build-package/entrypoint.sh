a01f273fb0744510e2060b201344d0c38ab86757
Brenek Harrison <brenekharrison@gmail.com>
2023-05-18 15:43:23 -0600

    .

--- build-package/entrypoint.sh
+++ build-package/entrypoint.sh
@@ -6,7 +6,8 @@ sudo chmod 666 $GITHUB_OUTPUT
 python -u /buildpkg.py "$@"
 
 ls -aR ~
-if [ -d "~/.cache/paru" ]; then
+if [[ -d "~/.cache/paru" ]]; then
+	echo "In the if"
 	ls -R ~/.cache/paru/clone
 	sudo cp ~/.cache/paru/clone/*/*.pkg.tar.zst $GITHUB_WORKSPACE/
 fi
