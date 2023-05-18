46bf2f47e03ce69a9160d27bb611d7d2997819a4
Brenek Harrison <brenekharrison@gmail.com>
2023-05-18 15:51:31 -0600

    Remove the fucking directory check
    Hopefully this fixes the problem

--- build-package/entrypoint.sh
+++ build-package/entrypoint.sh
@@ -5,9 +5,5 @@ sudo chmod 666 $GITHUB_OUTPUT
 
 python -u /buildpkg.py "$@"
 
-ls -aR ~
-if [[ -d "~/.cache/paru" ]]; then
-	echo "In the if"
-	ls -R ~/.cache/paru/clone
-	sudo cp ~/.cache/paru/clone/*/*.pkg.tar.zst $GITHUB_WORKSPACE/
-fi
+ls -R ~/.cache/paru/clone
+sudo cp ~/.cache/paru/clone/*/*.pkg.tar.zst $GITHUB_WORKSPACE/
