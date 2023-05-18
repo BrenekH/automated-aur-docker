a97debfa8fb34bf81bdc89d16cf9ca87b0e08415
Brenek Harrison <brenekharrison@gmail.com>
2023-05-18 14:56:19 -0600

    Double install to ensure dependencies are installed

--- test-package/entrypoint.sh
+++ test-package/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 pacman -Syu
+
+# We "install" the built packages twice, once without dependency checks and once with, so
+# that if the shared package cache contains packages that depend on each other, Pacman will
+# still install the packages in the cache, and the package dependencies (using the second)
+# install command.
 pacman -Udd --noconfirm $GITHUB_WORKSPACE/*.pkg.tar.zst
+pacman -U --noconfirm $GITHUB_WORKSPACE/*.pkg.tar.zst
 
 python -u /testpkg.py "$@"
