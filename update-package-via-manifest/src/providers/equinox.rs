a29339f6ae001d7625d4851dd6a25a2cc4023980
Brenek Harrison <brenekharrison@gmail.com>
2026-07-21 17:21:03 -0600

    Finish implementing EquinoxProvider.get_update_data()

--- update-package-via-manifest/src/providers/equinox.rs
+++ update-package-via-manifest/src/providers/equinox.rs
@@ -1,6 +1,7 @@
-use std::any::Any;
+use std::{any::Any, collections::HashMap};
 
 use anyhow::anyhow;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
@@ -44,7 +45,29 @@ impl UpdateProvider for EquinoxProvider {
             &self.latest_version(manifest_data)?
         };
 
-        todo!()
+        let mut update_data = UpdateData {
+            update_checksums: true,
+            ..Default::default()
+        };
+        let mut arch_map = HashMap::from([
+            // The period at the end of each key is the beginning of the file extension in the URL.
+            // It is used to ensure arm doesn't match the arm64 url.
+            ("amd64.", &mut update_data.source_x86_64),
+            ("386.", &mut update_data.source_i686),
+            ("arm64.", &mut update_data.source_aarch64),
+            ("arm.", &mut update_data.source_armv7h),
+        ]);
+
+        let urls = Self::get_linux_source_urls(&data.app_slug, latest_version)?;
+        for url in urls {
+            for (equinox_arch, data_pointer) in &mut arch_map {
+                if url.contains(equinox_arch) {
+                    **data_pointer = Some(vec![url.clone()]);
+                }
+            }
+        }
+
+        Ok(update_data)
     }
 }
 
@@ -54,13 +77,30 @@ impl EquinoxProvider {
             latest_version: None,
         }
     }
+
+    fn get_linux_source_urls(app_slug: &str, app_version: &str) -> anyhow::Result<Vec<String>> {
+        let regx = Regex::new(&format!(
+            r"https:\/\/bin\.equinox\.io\/.*\/.*-{}-linux-.*\.tar\.gz",
+            app_version.replace('.', r"\.")
+        ))?;
+
+        let response = ureq::get(format!("https://dl.equinox.io/{app_slug}/stable/archive"))
+            .call()?
+            .body_mut()
+            .read_to_string()?;
+
+        Ok(regx
+            .find_iter(&response)
+            .map(|x| x.as_str().to_string())
+            .collect())
+    }
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct EquinoxData {
-    #[serde(rename="appID")]
+    #[serde(rename = "appID")]
     pub app_id: String,
-    #[serde(rename="appSlug")]
+    #[serde(rename = "appSlug")]
     pub app_slug: String,
 }
 
