79e77ddeb73b1587b953a773c8047635b2812eb2
Brenek Harrison <brenekharrison@gmail.com>
2026-07-30 22:55:05 -0600

    Plan the publish action Rust rewrite

---
+++ publish-package/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn publish() {
+    // Read manifest
+
+    // Clone existing AUR repo into temp dir
+
+    // Configure Git (user.name and user.email)
+
+    // Copy PKGBUILD and manifest.include files to cloned repo
+
+    // Create .SRCINFO file in cloned repo
+
+    // Overwrite .gitignore
+
+    // Force add PKGBUILD, .SRCINFO, .gitignore, and mani.include files.
+
+    // Commit files to cloned repo
+
+    // Push new commit to AUR
+}
---
+++ publish-package/src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+
+use common::gha_subscriber::GHALayer;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+fn main() {
+    tracing_subscriber::registry()
+        .with(GHALayer {})
+        .with(LevelFilter::DEBUG)
+        .init();
+
+    let args: Vec<String> = env::args().collect();
+
+    let target_pkg_dir = args
+        .get(1)
+        .expect("Expected a package directory as the first argument.");
+}
